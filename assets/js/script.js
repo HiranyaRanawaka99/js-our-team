19dae26b6cacc5f15f0972ecbbfb195bc64aa0fa
HiranyaRanawaka99 <hiranya.ranawaka99@gmail.com>
2023-08-24 18:08:32 +0300

    add: millestone 1

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+
+
+const team = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        foto: 'barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        foto: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg',
+    }
+]
+
+//MILLESTONE 1
+console.log(team[0]);
+console.log(team[1]);
+console.log(team[2]);
+console.log(team[3]);
+console.log(team[4]);
+console.log(team[5]);
+
